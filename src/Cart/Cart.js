0431040c7544338c4706f69372ddd7c0f9bc3432
Your Name <you@example.com>
2021-12-13 22:39:24 +0600

    ok baby

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -6,7 +6,6 @@ const Cart = (props) => {
     //total price calculate here...
     let total = 0;
     let totalQuantity = 0;
-
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
 
@@ -19,12 +18,18 @@ const Cart = (props) => {
         totalQuantity += product.quantity;
 
     }
+    const shipping = total > 0 ? 50 : 0;
+    const tax = (total + shipping) * 0.10;
+    const grandTotal = total + shipping + tax;
 
     return (
         <div>
             <h2>Order Summary</h2>
             <h3>Item Ordered : {totalQuantity}</h3>
             <h3>Total: {total.toFixed(2)}</h3>
+            <h3>Tax: {tax.toFixed(2)}</h3>
+            <h3>Shipping: {shipping.toFixed(2)}</h3>
+            <h3>Grand Total: {grandTotal.toFixed(2)}</h3>
         </div>
     );
 };
